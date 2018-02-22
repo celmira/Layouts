ef01059d0945483573c1c31213fa54916c68f6b1
celmira <celmira@unicauca.edu.co>
2018-02-21 19:31:44 -0500

    plantilla_linear puede servir de referencia para las demas

--- app/src/main/java/com/example/celmiramedina/comentarios/MainActivity.java
+++ app/src/main/java/com/example/celmiramedina/comentarios/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inicio);
+        setContentView(R.layout.plantilla_linear);
 
        /* final RatingBar mRatingBar = (RatingBar) findViewById(R.id.ratingBarEstrellas);
         final TextView mRatingScale = (TextView) findViewById(R.id.mensajeEscala);
