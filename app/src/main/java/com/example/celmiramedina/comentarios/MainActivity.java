a154ab54d6592cc8aa3e938052d888f45756326e
Andrea Paz <yuranipaz@unicauca.edu.co>
2018-03-02 20:49:40 -0500

    layouts Andrea Paz

--- app/src/main/java/com/example/celmiramedina/comentarios/MainActivity.java
+++ app/src/main/java/com/example/celmiramedina/comentarios/MainActivity.java
@@ -2,19 +2,13 @@ package com.example.celmiramedina.comentarios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RatingBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plantilla_linear);
+        setContentView(R.layout.comentarios_organizador);
 
        /* final RatingBar mRatingBar = (RatingBar) findViewById(R.id.ratingBarEstrellas);
         final TextView mRatingScale = (TextView) findViewById(R.id.mensajeEscala);
